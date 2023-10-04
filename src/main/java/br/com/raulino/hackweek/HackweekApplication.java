865c8d8ecf8440b9bad4eb94653fd1c8acf2bb1a
R4UL1N0 <raulino.storage@gmail.com>
2023-10-04 16:06:56 -0300

    adding spring boot initial template

---
+++ src/main/java/br/com/raulino/hackweek/HackweekApplication.java
@@ -0,0 +1,13 @@
+package br.com.raulino.hackweek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackweekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackweekApplication.class, args);
+	}
+
+}
